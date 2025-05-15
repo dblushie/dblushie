0a8f84e71c117b97622308d86d03558785b62760
dblushie <27.restianiauliaputri.xg@gmail.com>
2025-05-15 15:16:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("beruang kutub")
 st.write(
     "beruang kutub gemes imut lucu."
 )
-st.image(ee5a551e64a7f75074e7e8d9f67c5a5f.jpg",width=200)
+st.image("ee5a551e64a7f75074e7e8d9f67c5a5f.jpg",width=200)
