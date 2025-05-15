9232c080cd2c7ecd8c6888823deaab397b9e9e2d
dblushie <27.restianiauliaputri.xg@gmail.com>
2025-05-15 15:14:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("beruang kutub🎈")
+st.title("beruang kutub")
 st.write(
     "beruang kutub gemes imut lucu."
 )
+st.image(ee5a551e64a7f75074e7e8d9f67c5a5f.jpg",width=200)
