ac30de127a3e2bec69951cd0c710d405c5e668c9
dblushie <27.restianiauliaputri.xg@gmail.com>
2025-05-22 14:48:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,10 @@ st.write(
     "beruang kutub gemes imut lucu."
 )
 st.image("ee5a551e64a7f75074e7e8d9f67c5a5f.jpg",width=200)
+st.header("nilai genap ganjil")
+angka = st.number_input("Tulis sebuah angka:", value=0, step=1) 
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap") 
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
+
